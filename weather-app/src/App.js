1d58ff114bb425774413f0bf743815fbf795918c
Andrea  Piazza <apiazza023@gmail.com>
2018-10-31 19:24:11 -0700

    API call to openWeather

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
 import Titles from "./components/Titles"
+import Form from "./components/Form"
+import Weather from "./components/Weather"
 
 // initialize component
+
+const API_Key = process.env.API_KEY
 
 
 //App is the major component - we will add smaller components and render them
 class App extends React.Component{
+  getWeather = async () => {
+    const api_call = await fetch(`https://samples.openweathermap.org/data/2.5/forecast?zip=94158&appid=${API_Key}`);
+
+    const data = await api_call.json();
+
+    console.log(data);
+  }
   render(){
     return(
       // returns jsx - We need Babel to run jsx for the browser to understand 
       //You can ONLY return one parent element - you only get one div per JSX
       <div>
-        <p>Hello!</p>
-        <h1>YO </h1>
+        <Titles />
+        {/* form component has access to props in form js file */}
+        <Form getWeather={this.getWeather}/>
+        <Weather />
       </div>
     );
   }
